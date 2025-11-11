ac9255ae291b570f24470cd981d4d1302a5fb513
eiis2id2idi <luannaim3991136@gmail.com>
2025-11-11 13:37:15 -0300

    Add BRAINROT dictionary with definitions and examples

---
+++ words.py
@@ -0,0 +1,10 @@
+BRAINROT = {
+    "mog": {
+        "definition": "Term meaning to dominate or impress someone in a chaotic or humorous way.",
+        "example": "He totally mogged everyone at the party with his skibidi moves."
+    },
+    "skibidi": {
+        "definition": "Random fun word used in Brainrot culture.",
+        "example": "Skibidi bop yes yes, stay based."
+    }
+}
